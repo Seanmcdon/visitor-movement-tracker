9465b776517e47dd84d3b527f2b6b302c6344149
Seanmcdon <Seanmichaelmcdonald@gmail.com>
2017-09-05 12:21:23 -0400

    Update sendMeAnEmail.php

--- sendMeAnEmail.php
+++ sendMeAnEmail.php
@@ -4,4 +4,4 @@ if(isset($_POST["str"])) $str = $_POST["str"]; else die();
 
 $emailSubject = $str;
 $emailBody = "";
-mail('seanmichaelmcdonald@gmail.com', $emailSubject, $emailBody);
+mail('/* email goes here BETWEEN quotes, delete stars and slashes */', $emailSubject, $emailBody);
